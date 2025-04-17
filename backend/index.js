e235d2725bbac0390ef626ab629a34d66eb7d95c
rios805 <157561849+rios805@users.noreply.github.com>
2025-04-16 21:14:17 -0700

    Setup Initial DB structure

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const cors = require("cors");
+const pool = require("./db"); //Using the DB connection from fastcomet
+require("dotenv").config(); //to load up env variables
 
 const app = express();
 const PORT = 3000;
 
 app.use(cors());
 app.use(express.json());
+
+
+// Testing DB connection
+app.get("/test-db", (req, res) => {
+	pool.query("SELECT 1 + 1 AS solution", (err, results) => {
+	  if (err) {
+		console.error("DB error:", err);
+		return res.status(500).json({ error: "Database connection failed" });
+	  }
+	  res.json({ message: "Database connected!", result: results[0].solution });
+	});
+  });
 
 //Here are some routes I created as placeholders 
 app.get("/", (req, res) => {
